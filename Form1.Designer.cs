97a4b3294e0326e9b20c11e7ef4baa97a6c0e430
MinJyun <84430808+MinJyun@users.noreply.github.com>
2021-11-22 10:51:12 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace 計數器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ShowLoadingPath = new System.Windows.Forms.TextBox();
+            this.OutPutFile = new System.Windows.Forms.Button();
+            this.showLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "載入清單";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ShowLoadingPath
+            // 
+            this.ShowLoadingPath.Location = new System.Drawing.Point(12, 19);
+            this.ShowLoadingPath.Name = "ShowLoadingPath";
+            this.ShowLoadingPath.Size = new System.Drawing.Size(328, 22);
+            this.ShowLoadingPath.TabIndex = 2;
+            // 
+            // OutPutFile
+            // 
+            this.OutPutFile.Location = new System.Drawing.Point(441, 12);
+            this.OutPutFile.Name = "OutPutFile";
+            this.OutPutFile.Size = new System.Drawing.Size(89, 33);
+            this.OutPutFile.TabIndex = 3;
+            this.OutPutFile.Text = "輸出檔案";
+            this.OutPutFile.UseVisualStyleBackColor = true;
+            this.OutPutFile.Click += new System.EventHandler(this.OutPutFile_Click);
+            // 
+            // showLabel
+            // 
+            this.showLabel.AutoSize = true;
+            this.showLabel.Location = new System.Drawing.Point(573, 22);
+            this.showLabel.Name = "showLabel";
+            this.showLabel.Size = new System.Drawing.Size(0, 12);
+            this.showLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 463);
+            this.Controls.Add(this.showLabel);
+            this.Controls.Add(this.OutPutFile);
+            this.Controls.Add(this.ShowLoadingPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "清點單";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ShowLoadingPath;
+        private System.Windows.Forms.Button OutPutFile;
+        private System.Windows.Forms.Label showLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 計數器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 計數 
+        /// </summary>
+        private Label[] counter;
+
+        /// <summary>
+        /// 品項名稱
+        /// </summary>
+        private Label[] itemsName;
+
+        /// <summary>
+        /// 目錄名稱
+        /// </summary>
+        private Label[] catalog;
+
+        /// <summary>
+        /// 讀取的目錄名稱
+        /// </summary>
+        private List<string> cataName = new List<string>();
+
+        /// <summary>
+        /// 紀錄該目錄名稱在第幾行
+        /// </summary>
+        private List<int> cataNumber = new List<int>();
+
+        /// <summary>
+        /// 加法按鈕功能
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void ButtonsPlus_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse((sender as Button).Name);
+            int count = int.Parse(counter[number].Text);
+            count++;
+            counter[number].Text = $"{count}";
+        }
+
+        /// <summary>
+        /// 減法功能
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void ButtonsMinus_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse((sender as Button).Name);
+            int count = int.Parse(counter[number].Text);
+            count--;
+            counter[number].Text = $"{count}";
+        }
+
+        /// <summary>
+        /// 載入檔案按鈕
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //選擇檔案
+                OpenFileDialog itemListFile = new OpenFileDialog();
+                itemListFile.ShowDialog();
+                ShowLoadingPath.Text = itemListFile.FileName;
+                List<string> items = LoadFile(ShowLoadingPath.Text);
+                int number = items.Count();
+
+                //控制元件的初始設定
+                Button[] buttonsPlus = new Button[number];
+                Button[] buttonsMinus = new Button[number];
+                itemsName = new Label[number];
+                counter = new Label[number];
+                catalog = new Label[cataName.Count()];
+
+                int front = 0;
+                int last = 0;
+                //分類數目
+                int j = 0;
+                foreach (var cata in cataName)
+                {
+                    front = cataNumber[j];
+                    last = cataNumber[j+1];
+                    catalog[j] = new Label();
+                    catalog[j].Name = "Cata" + j;
+                    catalog[j].Text = cata;
+                    catalog[j].Location = new Point(10 + 180*j, 60);
+                    for (int i = front; i < last; i++)
+                    {
+                        buttonsPlus[i] = new Button();
+                        buttonsPlus[i].Name = $"{i}";
+                        buttonsPlus[i].Text = "+";
+                        buttonsPlus[i].Location = new Point(100 + 180 * j, 80 + 30 * (i-front));
+                        buttonsPlus[i].Width = 25;
+
+                        buttonsMinus[i] = new Button();
+                        buttonsMinus[i].Name = $"{i}";
+                        buttonsMinus[i].Text = "-";
+                        buttonsMinus[i].Location = new Point(125 + 180 * j, 80 + 30 * (i - front));
+                        buttonsMinus[i].Width = 25;
+
+                        itemsName[i] = new Label();
+                        itemsName[i].Name = "Item" + i;
+                        itemsName[i].Text = items[i];
+                        itemsName[i].Location = new Point(10 + 180 * j, 85 + 30 * (i - front));
+
+                        counter[i] = new Label();
+                        counter[i].Name = "Counter" + i;
+                        counter[i].Text = "0";
+                        counter[i].Location = new Point(70 + 180 * j, 85 + 30 * (i - front));
+
+                        buttonsPlus[i].Click += new EventHandler(ButtonsPlus_Click);
+                        buttonsMinus[i].Click += new EventHandler(ButtonsMinus_Click);
+                    }
+                    j++;
+                }
+                this.Controls.AddRange(buttonsPlus);
+                this.Controls.AddRange(catalog);
+                this.Controls.AddRange(buttonsMinus);
+                this.Controls.AddRange(counter);
+                this.Controls.AddRange(itemsName);
+                showLabel.Text = "讀取完成";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("請選擇正確的檔案");
+            }
+        }
+
+        // 讀取檔案資料，並且存為類別list的function
+        public List<string> LoadFile(string File)
+        {
+            List<string> items = new List<string>();
+            //Loading File
+            string everyLine;
+            StreamReader file = new StreamReader(@File);
+            while ((everyLine = file.ReadLine()) != null)
+            {
+                if (everyLine.Contains(":"))
+                {
+                    cataName.Add(everyLine.Replace(":", string.Empty));
+                    cataNumber.Add(items.Count());
+                }
+                else if(everyLine != string.Empty)
+                {
+                    items.Add(everyLine);
+                }
+            }
+            cataNumber.Add(items.Count());
+            return items;
+        }
+
+        /// <summary>
+        /// 存檔
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OutPutFile_Click(object sender, EventArgs e)
+        {
+            string docPath = @"D:\";
+            //抓取今日時間
+            string today = DateTime.Now.ToString("yyyyMMdd");
+            string file = $"品項,數量{Environment.NewLine}";
+            for(int i = 0; i < itemsName.Count(); i++)
+            {
+                file = $"{file}{itemsName[i].Text},{counter[i].Text}{Environment.NewLine}";
+            }
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, $"{today}.csv"), false, Encoding.UTF8))
+            {
+                outputFile.WriteLine(file);
+            }
+            showLabel.Text = "存檔完成";
+        }
+    }
+}
